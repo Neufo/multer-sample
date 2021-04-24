22fb331904421aff534f0ec84a90001aaf8fe084
manabe-t <manabetakumi02@gmail.com>
2021-04-24 22:13:03 +0900

    clean

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-import express from "express";
-import multer from "multer";
-import path from "path";
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./uploads");
+        cb(null, './uploads');
     },
     filename: (req, file, callBack) => {
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
         const ext = path.extname(file.originalname);
-        callBack(null, file.fieldname + "-" + uniqueSuffix + ext);
+        callBack(null, file.fieldname + '-' + uniqueSuffix + ext);
     },
 });
 
 var upload = multer({ storage: storage });
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.post("/icons", upload.single("icon"), function (req, res, next) {
+app.post('/icons', upload.single('icon'), function (req, res, next) {
     res.status(200).send();
 });
 
-app.listen(3000, "localhost", () => {
-    console.log("server start.");
+app.listen(3000, 'localhost', () => {
+    console.log('server start.');
 });
